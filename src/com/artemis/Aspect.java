245aa8d535d8830052565fc1aeb71d254f9f57af
arni <arni@FURY>
2012-09-02 00:33:03 +0000

    Added "one" feature to Aspects. Now possible to define an aspect that processes entities that possess at least one of the specified component types.

--- src/com/artemis/Aspect.java
+++ src/com/artemis/Aspect.java
@@ -2,34 +2,76 @@ package com.artemis;
 
 import java.util.BitSet;
 
+/**
+ * An Aspects is used by systems as a matcher against entities, to check if a system is
+ * interested in an entity. Aspects define what sort of component types an entity must
+ * possess, or not possess.
+ * 
+ * This creates an aspect where an entity must possess A and B and C:
+ * Aspect.getAspectForAll(A.class, B.class, C.class)
+ * 
+ * This creates an aspect where an entity must possess A and B and C, but must not possess U or V.
+ * Aspect.getAspectForAll(A.class, B.class, C.class).exclude(U.class, V.class)
+ * 
+ * This creates an aspect where an entity must possess A and B and C, but must not possess U or V, but must possess one of X or Y or Z.
+ * Aspect.getAspectForAll(A.class, B.class, C.class).exclude(U.class, V.class).one(X.class, Y.class, Z.class)
+ *
+ * You can create and compose aspects in many ways:
+ * Aspect.getEmpty().one(X.class, Y.class, Z.class).all(A.class, B.class, C.class).exclude(U.class, V.class)
+ * is the same as:
+ * Aspect.getAspectForAll(A.class, B.class, C.class).exclude(U.class, V.class).one(X.class, Y.class, Z.class)
+ *
+ * @author Arni Arent
+ *
+ */
 public class Aspect {
 	
-	private BitSet bitSet;
+	private BitSet allSet;
 	private BitSet exclusionSet;
+	private BitSet oneSet;
 	
 	private Aspect() {
-		this.bitSet = new BitSet();
+		this.allSet = new BitSet();
 		this.exclusionSet = new BitSet();
+		this.oneSet = new BitSet();
 	}
 	
-	public BitSet getBitSet() {
-		return bitSet;
+	protected BitSet getAllSet() {
+		return allSet;
 	}
 	
-	public BitSet getExclusionSet() {
+	protected BitSet getExclusionSet() {
 		return exclusionSet;
 	}
 	
-	public Aspect add(Class<? extends Component> type, Class<? extends Component>... types) {
-		bitSet.set(ComponentType.getIndexFor(type));
+	protected BitSet getOneSet() {
+		return oneSet;
+	}
+	
+	/**
+	 * Returns an aspect where an entity must possess all of the specified component types.
+	 * @param type a required component type
+	 * @param types a required component type
+	 * @return an aspect that can be matched against entities
+	 */
+	public Aspect all(Class<? extends Component> type, Class<? extends Component>... types) {
+		allSet.set(ComponentType.getIndexFor(type));
 		
 		for (Class<? extends Component> t : types) {
-			bitSet.set(ComponentType.getIndexFor(t));
+			allSet.set(ComponentType.getIndexFor(t));
 		}
 
 		return this;
 	}
 	
+	/**
+	 * Excludes all of the specified component types from the aspect. A system will not be
+	 * interested in an entity that possesses one of the specified exclusion component types.
+	 * 
+	 * @param type component type to exclude
+	 * @param types component type to exclude
+	 * @return an aspect that can be matched against entities
+	 */
 	public Aspect exclude(Class<? extends Component> type, Class<? extends Component>... types) {
 		exclusionSet.set(ComponentType.getIndexFor(type));
 		
@@ -39,19 +81,72 @@ public class Aspect {
 		return this;
 	}
 	
-	
-	public static Aspect getAspectFor(Class<? extends Component> type, Class<? extends Component>... types) {
-		Aspect aspect = new Aspect();
-		
-		aspect.add(type);
+	/**
+	 * Returns an aspect where an entity must possess one of the specified component types.
+	 * @param type one of the types the entity must possess
+	 * @param types one of the types the entity must possess
+	 * @return an aspect that can be matched against entities
+	 */
+	public Aspect one(Class<? extends Component> type, Class<? extends Component>... types) {
+		oneSet.set(ComponentType.getIndexFor(type));
 		
 		for (Class<? extends Component> t : types) {
-			aspect.add(t);
+			oneSet.set(ComponentType.getIndexFor(t));
 		}
-		
+		return this;
+	}
+	
+	/**
+	 * Creates an aspect where an entity must possess all of the specified component types.
+	 * 
+	 * @param type the type the entity must possess
+	 * @param types the type the entity must possess
+	 * @return an aspect that can be matched against entities
+	 * 
+	 * @deprecated
+	 * @see getAspectForAll
+	 */
+	public static Aspect getAspectFor(Class<? extends Component> type, Class<? extends Component>... types) {
+		return getAspectForAll(type, types);
+	}
+	
+	/**
+	 * Creates an aspect where an entity must possess all of the specified component types.
+	 * 
+	 * @param type a required component type
+	 * @param types a required component type
+	 * @return an aspect that can be matched against entities
+	 */
+	public static Aspect getAspectForAll(Class<? extends Component> type, Class<? extends Component>... types) {
+		Aspect aspect = new Aspect();
+		aspect.all(type, types);
 		return aspect;
 	}
 	
+	/**
+	 * Creates an aspect where an entity must possess one of the specified component types.
+	 * 
+	 * @param type one of the types the entity must possess
+	 * @param types one of the types the entity must possess
+	 * @return an aspect that can be matched against entities
+	 */
+	public static Aspect getAspectForOne(Class<? extends Component> type, Class<? extends Component>... types) {
+		Aspect aspect = new Aspect();
+		aspect.one(type, types);
+		return aspect;
+	}
+	
+	/**
+	 * Creates and returns an empty aspect. This can be used if you want a system that processes no entities, but
+	 * still gets invoked. Typical usages is when you need to create special purpose systems for debug rendering,
+	 * like rendering FPS, how many entities are active in the world, etc.
+	 * 
+	 * You can also use the all, one and exclude methods on this aspect, so if you wanted to create a system that
+	 * processes only entities possessing just one of the components A or B or C, then you can do:
+	 * Aspect.getEmpty().one(A,B,C);
+	 * 
+	 * @return an empty Aspect that will reject all entities.
+	 */
 	public static Aspect getEmpty() {
 		return new Aspect();
 	}
--- src/com/artemis/EntitySystem.java
+++ src/com/artemis/EntitySystem.java
@@ -17,21 +17,32 @@ import com.artemis.utils.ImmutableBag;
 public abstract class EntitySystem implements EntityObserver {
 	private final int systemIndex;
 
-	private BitSet systemAspect;
-	
-	private BitSet exclusionAspect;
-
 	protected World world;
 
 	private Bag<Entity> actives;
-	
+
+	private Aspect aspect;
+
+	private BitSet allSet;
+	private BitSet exclusionSet;
+	private BitSet oneSet;
+
 	private boolean passive;
+
+	private boolean dummy;
 	
+	/**
+	 * Creates an entity system that uses the specified aspect as a matcher against entities.
+	 * @param aspect to match against entities
+	 */
 	public EntitySystem(Aspect aspect) {
 		actives = new Bag<Entity>();
-		systemAspect = aspect.getBitSet();
-		exclusionAspect = aspect.getExclusionSet();
+		this.aspect = aspect;
+		allSet = aspect.getAllSet();
+		exclusionSet = aspect.getExclusionSet();
+		oneSet = aspect.getOneSet();
 		systemIndex = SystemIndexManager.getIndexFor(this.getClass());
+		dummy = allSet.isEmpty() && oneSet.isEmpty(); // This system can't possibly be interested in any entity, so it must be "dummy"
 	}
 	
 	/**
@@ -90,29 +101,38 @@ public abstract class EntitySystem implements EntityObserver {
 	 * @param e entity to check
 	 */
 	protected final void check(Entity e) {
-		boolean systemContainsEntity = e.getSystemBits().get(systemIndex);
-		boolean systemIsInterestedInEntity = true;
-		
-		BitSet componentBits = e.getComponentBits();
-		for (int i = systemAspect.nextSetBit(0); i >= 0; i = systemAspect.nextSetBit(i+1)) {
-			if(!componentBits.get(i)) {
-				systemIsInterestedInEntity = false;
-				break;
-			}
+		if(dummy) {
+			return;
 		}
 		
-		if(!exclusionAspect.isEmpty()) {
-			for (int i = exclusionAspect.nextSetBit(0); i >= 0; i = exclusionAspect.nextSetBit(i+1)) {
-				if(componentBits.get(i)) {
-					systemIsInterestedInEntity = false;
+		boolean contains = e.getSystemBits().get(systemIndex);
+		boolean interested = true; // possibly interested, let's try to prove it wrong.
+		
+		BitSet componentBits = e.getComponentBits();
+
+		// Check if the entity possesses ALL of the components defined in the aspect.
+		if(!allSet.isEmpty()) {
+			for (int i = allSet.nextSetBit(0); i >= 0; i = allSet.nextSetBit(i+1)) {
+				if(!componentBits.get(i)) {
+					interested = false;
 					break;
 				}
 			}
 		}
+		
+		// Check if the entity possesses ANY of the exclusion components, if it does then the system is not interested.
+		if(!exclusionSet.isEmpty() && interested) {
+			interested = !exclusionSet.intersects(componentBits);
+		}
+		
+		// Check if the entity possesses ANY of the components in the oneSet. If so, the system is interested.
+		if(!oneSet.isEmpty()) {
+			interested = oneSet.intersects(componentBits);
+		}
 
-		if (systemIsInterestedInEntity && !systemContainsEntity && !systemAspect.isEmpty()) {
+		if (interested && !contains) {
 			insertToSystem(e);
-		} else if (!systemIsInterestedInEntity && systemContainsEntity && !systemAspect.isEmpty()) {
+		} else if (!interested && contains) {
 			removeFromSystem(e);
 		}
 	}
