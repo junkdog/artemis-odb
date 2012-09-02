7222f93f03160bf4c66549c2d46bdd030e601224
arni <arni@FURY>
2012-09-02 20:50:08 +0000

    Added comments.

--- src/com/artemis/systems/VoidEntitySystem.java
+++ src/com/artemis/systems/VoidEntitySystem.java
@@ -5,6 +5,14 @@ import com.artemis.Entity;
 import com.artemis.EntitySystem;
 import com.artemis.utils.ImmutableBag;
 
+/**
+ * This system has an empty aspect so it processes no entities, but it still gets invoked.
+ * You can use this system if you need to execute some game logic and not have to concern
+ * yourself about aspects or entities.
+ * 
+ * @author Arni Arent
+ *
+ */
 public abstract class VoidEntitySystem extends EntitySystem {
 
 	public VoidEntitySystem() {
