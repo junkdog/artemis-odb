a6f4750f8710baf36474e3bbee628456201c726a
lopho <zweistecken@lopho.org>
2013-09-28 19:28:15 +0200

    Added documentation for Bag and ImmutableBag. Fixed formatting (indent, whitespace, javadoc) inconsistency. Amend from last commit: Bag already implemented Iterable. doh.

--- src/com/artemis/utils/Bag.java
+++ src/com/artemis/utils/Bag.java
@@ -7,17 +7,28 @@ import java.util.NoSuchElementException;
 /**
  * Collection type a bit like ArrayList but does not preserve the order of its
  * entities, speedwise it is very good, especially suited for games.
+ * 
+ * @param <E>
  */
+public class Bag<E> implements ImmutableBag<E> {
 
-public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
-
+	/**
+	 * The backing array.
+	 */
 	E[] data;
+
+	/**
+	 * The elements contained in bag.
+	 */
 	private int size = 0;
+
+	/**
+	 * The iterator. It is only created once and reused when required.
+	 */
 	private BagIterator it;
 
 	/**
 	 * Constructs an empty Bag with an initial capacity of 64.
-	 * 
 	 */
 	public Bag() {
 		this(64);
@@ -27,7 +38,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * Constructs an empty Bag with the specified initial capacity.
 	 * 
 	 * @param capacity
-	 *            the initial capacity of Bag
+	 *			the initial capacity of Bag
 	 */
 	@SuppressWarnings("unchecked")
 	public Bag(int capacity) {
@@ -39,7 +50,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * overwriting it was last element then removing last element
 	 * 
 	 * @param index
-	 *            the index of element to be removed
+	 *			the index of element to be removed
 	 * @return element that was removed from the Bag
 	 */
 	public E remove(int index) {
@@ -74,8 +85,8 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * does this by overwriting it was last element then removing last element
 	 * 
 	 * @param e
-	 *            element to be removed from this list, if present
-	 * @return <tt>true</tt> if this list contained the specified element
+	 *			element to be removed from this list, if present
+	 * @return {@code true} if this list contained the specified element
 	 */
 	public boolean remove(E e) {
 		for (int i = 0; i < size; i++) {
@@ -95,9 +106,9 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * Check if bag contains this element.
 	 * 
 	 * @param e
-	 * @return
+	 * @return {@code true} if the bag contains this element
 	 */
-    @Override
+	@Override
 	public boolean contains(E e) {
 		for(int i = 0; size > i; i++) {
 			if(e == data[i]) {
@@ -112,7 +123,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * specified Bag.
 	 * 
 	 * @param bag
-	 *            Bag containing elements to be removed from this Bag
+	 *			Bag containing elements to be removed from this Bag
 	 * @return {@code true} if this Bag changed as a result of the call
 	 */
 	public boolean removeAll(ImmutableBag<E> bag) {
@@ -140,10 +151,10 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * Returns the element at the specified position in Bag.
 	 * 
 	 * @param index
-	 *            index of the element to return
+	 *			index of the element to return
 	 * @return the element at the specified position in bag
 	 */
-    @Override
+	@Override
 	public E get(int index) {
 		return data[index];
 	}
@@ -153,7 +164,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * 
 	 * @return the number of elements in this bag
 	 */
-    @Override
+	@Override
 	public int size() {
 		return size;
 	}
@@ -171,18 +182,18 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * Checks if the internal storage supports this index.
 	 * 
 	 * @param index
-	 * @return
+	 * @return {@code true} if the index is within bounds
 	 */
 	public boolean isIndexWithinBounds(int index) {
 		return index < getCapacity();
 	}
 
 	/**
-	 * Returns true if this list contains no elements.
+	 * Returns true if this bag contains no elements.
 	 * 
-	 * @return true if this list contains no elements
+	 * @return {@code true} if this bag contains no elements
 	 */
-    @Override
+	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
@@ -192,7 +203,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * increases the capacity of the bag.
 	 * 
 	 * @param e
-	 *            element to be added to this list
+	 *			element to be added to this list
 	 */
 	public void add(E e) {
 		// is size greater than capacity increase capacity
@@ -206,8 +217,10 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	/**
 	 * Set element at specified index in the bag.
 	 * 
-	 * @param index position of element
-	 * @param e the element
+	 * @param index
+	 *			position of element
+	 * @param e
+	 *			the element
 	 */
 	public void set(int index, E e) {
 		if(index >= data.length) {
@@ -217,18 +230,35 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 		data[index] = e;
 	}
 
+	/**
+	 * Increase the capacity of the bag.
+	 * Capacity will increase by (3/2)*capacity + 1.
+	 */
 	private void grow() {
 		int newCapacity = (data.length * 3) / 2 + 1;
 		grow(newCapacity);
 	}
-	
+
+	/**
+	 * Set the capacity of the bag.
+	 * If the new capacity is smaller than the current, an
+	 * {@link IndexOutOfBoundsException IndexOutOfBoundsException} is thrown.
+	 *
+	 * @param newCapacity
+	 */
 	@SuppressWarnings("unchecked")
 	private void grow(int newCapacity) {
 		E[] oldData = data;
 		data = (E[])new Object[newCapacity];
 		System.arraycopy(oldData, 0, data, 0, oldData.length);
 	}
-	
+
+	/**
+	 * Check if an item, if added at the given item will fit into the bag.
+	 * If not, the bag capacity will be increased to hold an item at the index.
+	 *
+	 * @param index
+	 */
 	public void ensureCapacity(int index) {
 		if(index >= data.length) {
 			grow(index*2);
@@ -239,7 +269,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * Removes all of the elements from this bag. The bag will be empty after
 	 * this call returns.
 	 */
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	public void clear() {
 		// new null array so gc can clean up old one
 		data = (E[])new Object[data.length];
@@ -247,7 +277,8 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	}
 
 	/**
-	 * Add all items into this bag. 
+	 * Add all items into this bag.
+	 *
 	 * @param items
 	 */
 	public void addAll(ImmutableBag<E> items) {
@@ -260,7 +291,7 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 	 * Returns this bag's underlying array. Use with care. 
 	 * 
 	 * @see Bag#size()
-	 * @return The underlying array.
+	 * @return the underlying array.
 	 */
 	public Object[] getData() {
 		return data;
@@ -275,7 +306,13 @@ public class Bag<E> implements ImmutableBag<E>, Iterable<E> {
 		
 		return it;
 	}
-	
+
+
+	/**
+	 * An Iterator for Bag.
+	 *
+	 * @see Iterator
+	 */
 	private final class BagIterator implements Iterator<E> {
 		private int cursor;
 		private boolean validCursorPos;
