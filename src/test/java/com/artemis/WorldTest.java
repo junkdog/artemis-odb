98e6624d17ebbb6f3bb89aae3a61c8af25f1e74f
Adrian Papari <blackrax@gmail.com>
2013-10-01 08:25:16 +0200

    Added bag test.

--- src/test/java/com/artemis/WorldTest.java
+++ src/test/java/com/artemis/WorldTest.java
@@ -1,6 +1,5 @@
 package com.artemis;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,11 +15,6 @@ public class WorldTest
 	public void setUp() throws Exception
 	{
 		world = new World();
-	}
-
-	@After
-	public void tearDown() throws Exception
-	{
 	}
 
 	@Test
---
+++ src/test/java/com/artemis/utils/BagTest.java
@@ -0,0 +1,20 @@
+package com.artemis.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BagTest
+{
+	@Test // see https://code.google.com/p/artemis-framework/issues/detail?id=7
+	public void set_element_and_ensure_size()
+	{
+		Bag<Integer> b = new Bag<Integer>();
+		for (int i = 0; 10 > i; i++) {
+			b.add(i);
+		}
+		
+		b.set(4, -10);
+		assertEquals(10, b.size());
+	}
+}
