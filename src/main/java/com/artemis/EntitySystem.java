3a29e4606e885a7122f054cb16f52b9f26ea112d
Adrian Papari <blackrax@gmail.com>
2013-10-25 22:07:16 +0200

    Make delayedDeletion non-static in EntitySystem.

--- src/main/java/com/artemis/EntitySystem.java
+++ src/main/java/com/artemis/EntitySystem.java
@@ -26,7 +26,7 @@ public abstract class EntitySystem implements EntityObserver {
 	/** Contains all entities processed by this system. */
 	private final Bag<Entity> actives;
 	/** Collects entites to be deleted from the system after processing. */
-	private static final WildBag<Entity> delayedDeletion = new WildBag<Entity>();
+	private final WildBag<Entity> delayedDeletion;
 	/** Component bits entities must possess for the system to be interested. */
 	private final BitSet allSet;
 	/** Component bits entities must not possess for the system to be interested. */
@@ -52,6 +52,7 @@ public abstract class EntitySystem implements EntityObserver {
 	 */
 	public EntitySystem(Aspect aspect) {
 		actives = new Bag<Entity>();
+		delayedDeletion = new WildBag<Entity>();
 		allSet = aspect.getAllSet();
 		exclusionSet = aspect.getExclusionSet();
 		oneSet = aspect.getOneSet();
