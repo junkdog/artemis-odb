d3861958ba83d460e239eb1529dd606ec03bd1d7
Adrian Papari <blackrax@gmail.com>
2013-11-17 15:51:03 +0100

    Last javadoc for annotations (#35).

--- artemis/src/main/java/com/artemis/annotations/package-info.java
+++ artemis/src/main/java/com/artemis/annotations/package-info.java
@@ -2,7 +2,13 @@
  * Annotations used by Artemis.
  * <p>
  * Most prominently the {@link Mapper} annotation for injecting
- * ComponentMappers into systems.
+ * ComponentMappers into systems and managers.
+ * </p>
+ * <p>
+ * The weaver annotations can transform normal {@link com.artemis.Component} classes
+ * into a more appropriate type without writing the required boilerplate. The
+ * <code>artemis-odb-maven-plugin</code> is required for performing such
+ * transformations.
  * </p>
  */
 package com.artemis.annotations;
