6397c7264400b574aa09335deb000791aae21825
Adrian Papari <blackrax@gmail.com>
2013-11-17 15:27:18 +0100

    javadoc...

--- artemis/src/main/java/com/artemis/annotations/PackedWeaver.java
+++ artemis/src/main/java/com/artemis/annotations/PackedWeaver.java
@@ -1,3 +1,13 @@
 package com.artemis.annotations;
 
+import com.artemis.Component;
+import com.artemis.PooledComponent;
+
+/**
+ * Transforms a {@link Component} into a {@link PooledComponent}. Component transformation
+ * takes place during the <code>artemis</code> goal defined in <code>artemis-odb-maven-plugin</code>.
+ * 
+ * @see <a href="https://github.com/junkdog/artemis-odb/wiki/Component%20Types">Component types</a>
+ *      on the wiki.
+ */
 public @interface PackedWeaver {}
