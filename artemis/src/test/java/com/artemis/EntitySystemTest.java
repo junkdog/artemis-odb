9ddc7fccd014dcb5bd8be2168ed715832073914b
Oliver B <olbartley@gmail.com>
2014-06-09 21:56:28 -0700

    EntitySystem is not interested in inactive entities.

---
+++ artemis/src/test/java/com/artemis/EntitySystemTest.java
@@ -0,0 +1,64 @@
+package com.artemis;
+;
+import com.artemis.utils.ImmutableBag;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by obartley on 6/9/14.
+ */
+public class EntitySystemTest {
+
+	@Test(expected = NoSuchElementException.class)
+	public void test_process_one_inactive() {
+		World w = new World();
+
+		w.setSystem(new IteratorTestSystem(0));
+		w.initialize();
+
+		Entity e = w.createEntity();
+		e.addComponent(new C());
+		e.changedInWorld();
+
+		w.process();
+	}
+
+	public void test_process_one_active() {
+		World w = new World();
+
+		w.setSystem(new IteratorTestSystem(1));
+		w.initialize();
+
+		Entity e = w.createEntity();
+		e.addComponent(new C());
+		e.addToWorld();
+		e.changedInWorld();
+
+		w.process();
+	}
+
+	public static class C extends Component {}
+
+	public static class IteratorTestSystem extends EntitySystem {
+		public int expectedSize;
+		public IteratorTestSystem(int expectedSize) {
+			super(Aspect.getAspectForAll(C.class));
+			this.expectedSize = expectedSize;
+		}
+
+		@Override
+		protected void processEntities(ImmutableBag<Entity> entities) {
+			assertEquals(expectedSize, entities.size());
+			entities.iterator().next();
+		}
+
+		@Override
+		protected boolean checkProcessing() {
+			return true;
+		}
+	}
+}
