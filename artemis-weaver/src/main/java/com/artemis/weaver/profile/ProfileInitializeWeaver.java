ee198344491aed488ae028501de7dd566e85c674
Adrian Papari <blackrax@gmail.com>
2014-07-22 01:05:53 +0200

    #88: removed commented code.

--- artemis-weaver/src/main/java/com/artemis/weaver/profile/ProfileInitializeWeaver.java
+++ artemis-weaver/src/main/java/com/artemis/weaver/profile/ProfileInitializeWeaver.java
@@ -20,17 +20,6 @@ class ProfileInitializeWeaver extends AdviceAdapter implements Opcodes {
 		String profiler = info.profilerClass.getInternalName();
 		String profileDescriptor = info.profilerClass.getDescriptor();
 		
-//		mv.visitVarInsn(ALOAD, 0);
-//		mv.visitTypeInsn(NEW, profiler);
-//		mv.visitInsn(DUP);
-//		mv.visitMethodInsn(INVOKESPECIAL, profiler, "<init>", "()V");
-//		mv.visitFieldInsn(PUTFIELD, systemName, "$profiler", profileDescriptor);
-//		
-//		mv.visitVarInsn(ALOAD, 0);
-//		mv.visitFieldInsn(GETFIELD, systemName, "$profiler", profileDescriptor);
-//		mv.visitVarInsn(ALOAD, 0);
-//		mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Object", "getClass", "()Ljava/lang/Class;");
-//		mv.visitMethodInsn(INVOKEVIRTUAL, profiler, "setTag", "(Ljava/lang/Object;)V");
 		mv.visitVarInsn(ALOAD, 0);
 		mv.visitTypeInsn(NEW, profiler);
 		mv.visitInsn(DUP);
