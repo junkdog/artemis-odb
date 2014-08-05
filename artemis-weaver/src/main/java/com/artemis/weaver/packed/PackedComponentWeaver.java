d2e280136d7456b53baade6774ae1a6b60357e13
Adrian Papari <blackrax@gmail.com>
2014-08-05 23:22:25 +0200

    #111: updated javadoc

--- artemis-weaver/src/main/java/com/artemis/weaver/packed/PackedComponentWeaver.java
+++ artemis-weaver/src/main/java/com/artemis/weaver/packed/PackedComponentWeaver.java
@@ -1,6 +1,7 @@
 package com.artemis.weaver.packed;
 
 
+import static com.artemis.meta.ClassMetadata.WeaverType.PACKED;
 import static com.artemis.meta.ClassMetadataUtil.instanceFields;
 
 import org.objectweb.asm.ClassVisitor;
@@ -8,6 +9,7 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 import com.artemis.meta.ClassMetadata;
+import com.artemis.meta.ClassMetadata.WeaverType;
 import com.artemis.weaver.ConstructorInvocationVisitor;
 
 public class PackedComponentWeaver extends ClassVisitor implements Opcodes{
@@ -23,7 +25,8 @@ public class PackedComponentWeaver extends ClassVisitor implements Opcodes{
 	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
 		MethodVisitor method = null;
 		if ("<init>".equals(name) && "()V".equals(desc)) {
-			if (instanceFields(meta).size() > 0) {
+			if (PACKED == meta.annotation && instanceFields(meta).size() > 0) {
+				// constructor is replaced, so drop the default one.
 				return null;
 			} else {
 				method = cv.visitMethod(access, name, desc, signature, exceptions);
--- artemis/src/main/java/com/artemis/PackedComponent.java
+++ artemis/src/main/java/com/artemis/PackedComponent.java
@@ -6,10 +6,15 @@ import java.nio.ByteBuffer;
  * Packs components into a memory-friendly storage, such as a primitive array or {@link ByteBuffer},
  * reuses the same instance for all entities.
  * <p>
- * Requires zero-argument constructor and that, calling the constructor must not change the
- * underlying component data - ie, it's the equivalent of a {@link #clone()}.
+ * Constructor must either be zero-length or take a {@link World} instance.
+ * Calling the constructor must not change the underlying component data - ie,
+ * it's the equivalent of a {@link #clone()}.
  */
 public abstract class PackedComponent extends Component {
 	protected abstract PackedComponent forEntity(Entity e);
 	protected abstract void reset();
+	
+	public static interface DisposedWithWorld {
+		void free(World world);
+	}
 }
