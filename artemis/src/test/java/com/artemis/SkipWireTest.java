c08944cbe5f01f3d61c6c41f7fd8b3f1bfe59174
DaanVanYperen <daan@mostlyoriginal.net>
2015-08-29 22:52:20 +0200

    Tests for `@SkipWire`.

---
+++ artemis/src/test/java/com/artemis/SkipWireTest.java
@@ -0,0 +1,122 @@
+package com.artemis;
+
+import com.artemis.annotations.SkipWire;
+import com.artemis.annotations.Wire;
+import com.artemis.component.ComponentX;
+import com.artemis.component.ComponentY;
+import com.artemis.managers.TagManager;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Daan van Yperen
+ */
+public class SkipWireTest {
+
+	@Test
+	public void ensure_skipped_field_skipped_in_wired_class() {
+
+		@Wire
+		class TestManager extends Manager {
+			private ComponentMapper<ComponentX> x;
+			@SkipWire
+			private ComponentMapper<ComponentY> y;
+		}
+
+
+		TestManager manager = new TestManager();
+		new World(new WorldConfiguration()
+				.setManager(manager));
+
+		assertNotNull(manager.x);
+		assertNull(manager.y);
+	}
+
+	@Test
+	public void ensure_superclass_skipped_field_skipped_in_wired_class() {
+
+		class SuperTestManager extends Manager {
+			protected ComponentMapper<ComponentX> x;
+			@SkipWire
+			protected ComponentMapper<ComponentY> y;
+		}
+
+		@Wire(injectInherited = true)
+		class TestManager extends SuperTestManager {
+		}
+
+
+		TestManager manager = new TestManager();
+		new World(new WorldConfiguration()
+				.setManager(manager));
+
+		assertNotNull(manager.x);
+		assertNull(manager.y);
+	}
+
+
+	@Test
+	public void ensure_skipped_superclass_skipped_in_wired_class() {
+
+		class RootTestManager extends Manager {
+			protected ComponentMapper<ComponentY> z;
+		}
+
+		@SkipWire
+		class SuperTestManager extends RootTestManager {
+			protected ComponentMapper<ComponentY> y;
+		}
+
+		@Wire(injectInherited = true)
+		class TestManager extends SuperTestManager {
+			protected ComponentMapper<ComponentX> x;
+		}
+
+
+		TestManager manager = new TestManager();
+		new World(new WorldConfiguration()
+				.setManager(manager));
+
+		assertNotNull(manager.z);
+		assertNull(manager.y);
+		assertNotNull(manager.x);
+	}
+
+	@Test
+	public void ensure_wire_supersedes_skipwire_on_type() {
+
+		@Wire @SkipWire
+		class TestManager extends Manager {
+			private ComponentMapper<ComponentX> x;
+		}
+
+
+		TestManager manager = new TestManager();
+		new World(new WorldConfiguration()
+				.setManager(manager));
+
+		assertNotNull(manager.x);
+	}
+
+	@Test
+	public void ensure_wire_supersedes_skipwire_on_field() {
+
+
+		class TestManager extends Manager {
+			@Wire @SkipWire
+			private ComponentMapper<ComponentX> x;
+		}
+
+
+		TestManager manager = new TestManager();
+		new World(new WorldConfiguration()
+				.setManager(manager));
+
+		assertNotNull(manager.x);
+	}
+
+}
