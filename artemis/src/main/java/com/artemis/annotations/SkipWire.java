844cc2063d6ec82ba33a523b7bb5ccf7a9de1bca
DaanVanYperen <daan@mostlyoriginal.net>
2015-08-29 22:46:55 +0200

    Introduce `@SkipWire` annotation.

---
+++ artemis/src/main/java/com/artemis/annotations/SkipWire.java
@@ -0,0 +1,19 @@
+package com.artemis.annotations;
+
+import com.artemis.*;
+
+import java.lang.annotation.*;
+
+
+/**
+ * Skip reflective dependency injection on annotated field or class.
+ *
+ * Allows excluding specific fields or classes in {@see Wire}d hierarchy.
+ *
+ * @author Daan van Yperen
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Documented
+public @interface SkipWire {
+}
