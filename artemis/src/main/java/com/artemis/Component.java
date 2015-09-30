ddafbf08c0bdfee259214a1b431f441a840c68f7
DaanVanYperen <daan@mostlyoriginal.net>
2015-09-30 20:41:42 +0200

    Javadoc.

--- artemis/src/main/java/com/artemis/Component.java
+++ artemis/src/main/java/com/artemis/Component.java
@@ -1,11 +1,14 @@
 package com.artemis;
 
 /**
- * The most basic component type.
- * 
- * @see PackedComponent
- * @see PooledComponent
- * 
+ * Components are pure data classes with optionally some helper methods.
+ * <p/>
+ * Extend to create your own components. Decorate with {@link com.artemis.annotations.PooledWeaver}
+ * or manually extend {@link PooledComponent} to make the component pooled.
+ *
  * @author Arni Arent
+ * @see PooledComponent
+ * @see com.artemis.annotations.PooledWeaver
  */
-public abstract class Component {}
+public abstract class Component {
+}
