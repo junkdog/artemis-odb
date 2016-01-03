1919b01540dc6afa1a205de8119c953cc9957453
DaanVanYperen <daan@mostlyoriginal.net>
2016-01-03 22:56:11 +0100

    Add regression test.

---
+++ artemis/src/test/java/com/artemis/WorldConfigurationBuilderInjectionTest.java
@@ -0,0 +1,56 @@
+package com.artemis;
+
+import com.artemis.annotations.Wire;
+import com.artemis.injection.FieldResolver;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author Daan van Yperen
+ */
+public class WorldConfigurationBuilderInjectionTest {
+
+	private WorldConfigurationBuilder builder;
+
+	@Before
+	public void setUp() throws Exception {
+		builder = new WorldConfigurationBuilder();
+	}
+
+	@Test
+	public void custom_field_resolvers_should_not_suppress_default_wiring() {
+
+		FieldResolver resolver = mock(FieldResolver.class);
+		CoopSystem coopSystem = new CoopSystem();
+		builder
+				.with(coopSystem)
+				.register(resolver);
+
+		WorldConfiguration configuration = builder.build();
+		Chicken chicken = new Chicken();
+		configuration.register(chicken);
+
+		new World(configuration);
+		assertEquals(chicken,coopSystem.myChicken);
+		assertNotNull(coopSystem.mFlaming);
+	}
+
+	public static class Chicken {};
+	public static class Flaming extends Component {};
+	public static class CoopSystem extends BaseSystem {
+
+		@Wire
+		public Chicken myChicken;
+		public ComponentMapper<Flaming> mFlaming;
+
+		@Override
+		protected void processSystem() {
+		}
+	}
+}
