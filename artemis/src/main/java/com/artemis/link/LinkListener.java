1ef6de14856e69211d74f76553a8e30d9df88dbb
Adrian Papari <a@onedaybeard.net>
2016-04-26 14:16:22 +0200

    #385: javadoc LinkListener

--- artemis/src/main/java/com/artemis/link/LinkListener.java
+++ artemis/src/main/java/com/artemis/link/LinkListener.java
@@ -1,8 +1,40 @@
 package com.artemis.link;
 
+/**
+ * Callbacks for links between entities.
+ */
 public interface LinkListener {
+
+	/**
+	 * Established connection between <code>sourceId:ComponentType:Field</code>
+	 * and <code>targetId</code>.
+	 *
+	 * @param sourceId entity owning component.
+	 * @param targetId a valid entity id.
+	 */
 	void onLinkEstablished(int sourceId, int targetId);
+
+	/**
+	 * Deletion of source entity or its component.
+	 *
+	 * @param sourceId entity owning component.
+	 */
 	void onLinkKilled(int sourceId);
+
+	/**
+	 * Target entity dead.
+	 *
+	 * @param sourceId entity owning component.
+	 * @param deadTargetId a valid entity id.
+	 */
 	void onTargetDead(int sourceId, int deadTargetId);
+
+	/**
+	 * Target entity has changed.
+	 *
+	 * @param sourceId entity owning component.
+	 * @param targetId a valid entity id.
+	 * @param oldTargetId previous entity id.
+	 */
 	void onTargetChanged(int sourceId, int targetId, int oldTargetId);
 }
