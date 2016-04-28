8a7797dd1581cfea94322d070131222f734f7fbd
Adrian Papari <a@onedaybeard.net>
2016-04-28 10:34:34 +0200

    #385: LinkPolicy javadoc

--- artemis/src/main/java/com/artemis/annotations/LinkPolicy.java
+++ artemis/src/main/java/com/artemis/annotations/LinkPolicy.java
@@ -6,6 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>Annotation for entity-referencing fields in component types. This annotation
+ * allows overriding the default link policy. Fields referencing a single
+ * entity - int or {@link com.artemis.Entity} - default to
+ * {@link Policy#CHECK_SOURCE_AND_TARGETS}, while Bag and IntBag of entities are
+ * assigned {@link Policy#CHECK_SOURCE}.</p>
+ *
+ *
+ * <p>This</p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
@@ -13,8 +21,27 @@ public @interface LinkPolicy {
 	Policy value();
 
 	enum Policy {
+		/**
+		 * Performs no validation on this field's entity reference(s).
+		 */
 		SKIP,
+
+		/**
+		 * Validates entity id.
+		 *
+		 * @see com.artemis.link.LinkListener#onLinkEstablished(int, int)
+		 * @see com.artemis.link.LinkListener#onLinkKilled(int, int)
+		 */
 		CHECK_SOURCE,
+
+		/**
+		 * <p>Validates source entity and any targets.
+		 * {@link com.artemis.link.LinkListener#onTargetChanged(int, int, int)} is
+		 * only invoked if the field deals with a single entity.</p>
+		 *
+		 *  @see com.artemis.link.LinkListener#onTargetChanged(int, int, int)
+		 *  @see com.artemis.link.LinkListener#onTargetDead(int, int)
+		 */
 		CHECK_SOURCE_AND_TARGETS
 	}
 }
