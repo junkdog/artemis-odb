40985f49eb3b86070515b5623a5331be9e436ad9
Adrian Papari <a@onedaybeard.net>
2016-05-11 13:38:06 +0200

    updated javadoc for `@EntityId`

--- artemis/src/main/java/com/artemis/annotations/EntityId.java
+++ artemis/src/main/java/com/artemis/annotations/EntityId.java
@@ -1,17 +1,28 @@
 package com.artemis.annotations;
 
+import com.artemis.Entity;
+import com.artemis.link.EntityLinkManager;
+import com.artemis.utils.Bag;
+import com.artemis.utils.IntBag;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Using this annotation, entity references can be safely serialized.
+ * <p>Marks <code>int</code> and {@link IntBag} fields as holding entity id:s.
+ * Only works on component types. This annotation ensures that:
+ * <ul>
+ *     <li>Entity references can be safely serialized</li>
+ *     <li>Tracks inter-entity relationships, if the {@link EntityLinkManager}
+ *         is registed with the world.</li>
+ * </ul>
  *
- * This feature is only for serialization, and does not protect your
- * references from dangling when entities go out of scope.
+ * <p>{@link Bag} of {@link Entity} and plain <code>Entity</code> fields
+ * don't need to be annotated.</p>
  *
- * see https://github.com/junkdog/artemis-odb/wiki/Entity-References-and-Serialization
+ * @see <a href="https://github.com/junkdog/artemis-odb/wiki/Entity-References-and-Serialization">Entity References and Serialization</a>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
